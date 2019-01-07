1826a8f6608675019d0751ff26ec3dc7c283e2bd
BaHbKAwrx <bahbkawrx@gmail.com>
2019-01-07 16:06:19 +0200

    Init commit

---
+++ English with Papich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  English with Papich
+//
+//  Created by Shmygovskii Ivan on 07.01.19.
+//  Copyright © 2019 Shmygovskii Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
